2847686f2ca99ec8a9c97a7c723397c87f8f01d9
Samodra <38998978+saamodra@users.noreply.github.com>
2021-01-30 23:14:10 +0700

    300121

--- BUSS/Content/assets/js/script.js
+++ BUSS/Content/assets/js/script.js
@@ -23,28 +23,3 @@ $(document).ready(function() {
     sticky.addClass('nav-sticky navbar-light');
   }
 })
-
-// $('.nav-item-2').click(function() {
-//   $('body').animate({
-//       scrollTop: $('#' + $(this).attr('target')).offset().top
-//   }, 1000);
-
-//   console.log('asd')
-// });
-
-// $('.nav-item-2, .footer-menu').on('click', function (e) {
-//   e.preventDefault();
-//   var to = $(this).attr('href');
-
-//   if(to == '#home') {
-//     $('html, body').animate({
-//       scrollTop: 0
-//     }, 1000);
-
-//   } else {
-
-//     $('html, body').animate({
-//       scrollTop: eval($(to).offset().top )
-//     }, 700);
-//   }
-// });
---
+++ BUSS/Controllers/LaporanController.cs
@@ -0,0 +1,27 @@
+﻿using BUSS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BUSS.Controllers
+{
+    public class LaporanController : Controller
+    {
+        BUSSEntities db = new BUSSEntities();
+
+        // GET: Laporan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pesanan()
+        {
+            var transaksi = db.Transaksis.ToList();
+
+            return View(transaksi);
+        }
+    }
+}
