6dd786373be2f0aba6e9be142461665f41954da7
Samodra <38998978+saamodra@users.noreply.github.com>
2021-02-01 22:05:38 +0700

    Dashboard Manager

--- BUSS/Controllers/DashboardController.cs
+++ BUSS/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BUSS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,9 @@ namespace BUSS.Controllers.Admin
 {
     public class DashboardController : Controller
     {
-        // GET: Dashboard
+        BUSSEntities db = new BUSSEntities();
 
+        // GET: Dashboard
         public ActionResult Index()
         {
             return View();
@@ -22,12 +24,18 @@ namespace BUSS.Controllers.Admin
 
         public ActionResult Tourleader()
         {
-
             return View();
         }
 
         public ActionResult Manager()
         {
+            ViewBag.grafikPenjualan = db.view_DashboardCustomer.ToList();
+            ViewBag.Transaksi = db.view_TransaksiCount.OrderByDescending(x => x.jumlah_dipesan).Take(5).ToList();
+            ViewBag.trCount = db.Transaksis.Count();
+            ViewBag.trSum = db.Transaksis.Sum(x => x.Harga_total);
+            ViewBag.JumlahDestinasiWilayah = db.view_JumlahDestinasiWilayah.OrderByDescending(x => x.JumlahDestinasi).Take(5).ToList();
+            ViewBag.DestinasiTerlaris = db.view_DestinasiTerlaris.OrderByDescending(x => x.JumlahDipesan).Take(5).ToList();
+
             return View();
         }
     }
